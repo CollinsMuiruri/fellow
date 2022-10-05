e7edd7dde01c0b7ec94f794a062fa2a2690c88e3
CollinsMuiruri <wanyekicollins@gmail.com>
2022-10-05 18:44:38 +0300

    Connect Railway Database

--- fellow/fellow/settings.py
+++ fellow/fellow/settings.py
@@ -76,12 +76,23 @@ WSGI_APPLICATION = 'fellow.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/4.1/ref/settings/#databases
 
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.sqlite3',
+#         'NAME': BASE_DIR / 'db.sqlite3',
+#     }
+# }
+
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'railway',
+        'USER': 'postgres',
+        'PASSWORD': 'L7Y8Ch8nxKxl7sLQ6pWe',
+        'HOST': 'containers-us-west-80.railway.app',
+        'PORT': '6375',
+        },
     }
-}
 
 
 # Password validation
