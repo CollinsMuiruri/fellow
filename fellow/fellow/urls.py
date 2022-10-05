59a107d94fadd3de0edf4809a95260559c27391c
CollinsMuiruri <wanyekicollins@gmail.com>
2022-10-05 18:01:07 +0300

    Seller app is connected

--- fellow/fellow/urls.py
+++ fellow/fellow/urls.py
@@ -17,6 +17,7 @@ from django.contrib import admin
 from django.urls import include, path
 
 urlpatterns = [
-    path('',include('buyer.urls')),
+    path('buyer/',include('buyer.urls')),
+    path('seller/',include('seller.urls')),
     path('admin/', admin.site.urls),
 ]
